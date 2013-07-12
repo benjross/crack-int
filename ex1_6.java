01a70a7b98bc006a27d441e8c17492be95f20f1f
Ben Ross <benjross@cs.washington.edu>
2013-07-12 15:54:05 -0700

    a version of ex1_6 completed

--- ex1_6.java
+++ ex1_6.java
@@ -1,16 +1,46 @@
 public class ex1_6 {
-    public static void rotate(int[][] array) {
-    }
-    public static void main(String[] args) {
-        int[][] matrix = new int[5][5];
-        int value = 0;
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(value + "\t");
-                matrix[i][j] = value++;
-            }
-            System.out.println();
-        }
-        
-    }
+	public static int[][] rotate(int[][] array) {
+		int n = array.length;
+		int[][] tmp = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				tmp[j][n - 1 - i] = array[i][j];
+			}
+		}	
+		return tmp;
+	}
+
+	public static void  rotateInPlace(int[][] array) {
+	}
+
+	public static void print(int[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				System.out.print(matrix[i][j] + "\t");
+			}
+			System.out.println();
+		}
+	}
+
+	public static int[][] generate(int n) {
+		int[][] matrix = new int[n][n];
+		int value = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				matrix[i][j] = value++;
+			}
+		}
+		return matrix;
+	}
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 6; i++) {
+			System.out.println("A " + i + " x " + i + " matrix:");
+			int[][] matrix = generate(i);
+			print(matrix);
+			System.out.println("\nRotated:");
+			print(rotate(matrix));
+			System.out.println();
+		}
+	}
 }
